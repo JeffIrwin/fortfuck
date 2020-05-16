c85a9d0a8f7d2e6cc8476e31024400ce4aa5fbbd
Jeff Irwin <jirwin505@gmail.com>
2020-05-16 18:43:54 -0400

    Add comments

--- test.sh
+++ test.sh
@@ -2,6 +2,13 @@
 
 rm samples/*/a.out
 
+# Testing the full fortfuck pipeline takes three stages:
+#
+#   1. Run fbc to compile brainfuck into assembly
+#   2. Run gcc to link the assembly
+#   3. Run the compiled and linked brainfuck executable a.out
+#
+
 ./testfbc.sh || exit
 ./testgcc.sh || exit
 ./testa.sh   || exit
